a3d54c539bf95b87a3e89750b32bd258ed346fb0
Khiem19 <102414499+Khiem19@users.noreply.github.com>
2024-08-25 14:06:50 +0200

    Time: 0 ms (100%), Space: 7.9 MB (27.65%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,7 +3,7 @@ public:
     int hammingWeight(int n) {
         int count = 0;
         while(n) {
-            count += n & 1;
+            count += n & 1; //bit wise automatically access to binary from of int number.!!
             n >>= 1;
         }
         return count;
