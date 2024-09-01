5acbfa34bb0450a45d5788cd7128e16a398ca8ff
Khiem19 <102414499+Khiem19@users.noreply.github.com>
2024-09-01 17:04:58 +0200

    Time: 120 ms (21.93%), Space: 22.1 MB (17.07%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,41 +1,38 @@
-// class NumArray {
-// public:
-//     vector<int> numArray;
-//     NumArray(vector<int>& nums) {
-//         for(int i = 0; i < nums.size(); i++) {
-//             numArray.push_back(nums[i]);
-//         }
-//     }
-    
-//     int sumRange(int left, int right) {
-//         int sum = 0;
-//         for(int i = left; i <= right; i++) {
-//             sum += numArray[i];
-//         }
-//         return sum;
-//     }
-// };
-
-
 class NumArray {
 public:
-    vector<int> memo = {0};
-    NumArray(vector<int> &nums) {
-        int sum = 0;
-        for (int n : nums) {
-            sum += n;
-            memo.push_back(sum);
+    vector<int> numArray;
+    NumArray(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++) {
+            numArray.push_back(nums[i]);
         }
     }
     
-    // sumRange(2, 4)=prefixSums[5]−prefixSums[2]
     int sumRange(int left, int right) {
-        return memo[right+1] - memo[left];
+        int sum = 0;
+        for(int i = left; i <= right; i++) {
+            sum += numArray[i];
+        }
+        return sum;
     }
 };
 
 
-
+// class NumArray {
+// public:
+//     vector<int> memo = {0};
+//     NumArray(vector<int> &nums) {
+//         int sum = 0;
+//         for (int n : nums) {
+//             sum += n;
+//             memo.push_back(sum);
+//         }
+//     }
+    
+//     // sumRange(2, 4)=prefixSums[5]−prefixSums[2]
+//     int sumRange(int left, int right) {
+//         return memo[right+1] - memo[left];
+//     }
+// };
 
 
 /**
